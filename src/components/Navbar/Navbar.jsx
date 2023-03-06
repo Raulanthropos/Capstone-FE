fc3454743b0ab79e097dadeb174c422a1bde0464
Raulanthropos <ipsichias@yandex.com>
2023-03-06 21:02:14 +0200

    minor fix

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -6,7 +6,7 @@ import Navbar from 'react-bootstrap/Navbar';
 function Logo() {
   return (
     <img
-      src="/public/images/logo.png"
+      src={process.env.REACT_APP_NEXT_PUBLIC_BASE_URL + "/images/logo.png"}
       width="100"
       height="30"
       className="d-inline-block align-top"
