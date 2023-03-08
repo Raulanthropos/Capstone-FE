5f6a80581d5997e2323ca01133ffdb4f0a38f07a
Raulanthropos <ipsichias@yandex.com>
2023-03-08 22:09:24 +0200

    Revert "register is complete, need to figure out why it doesn't connect to the BE"
    This reverts commit ff5191f36960f1f5be6e0f0ddc5d030b308ba56e.

--- src/components/Register/Register.jsx
+++ src/components/Register/Register.jsx
@@ -1,182 +1,71 @@
-import React, { Component } from "react";
-import { Form, Button, Container, Row, Col, Spinner } from "react-bootstrap";
-import { Link, useNavigate } from "react-router-dom";
-import { useState } from "react";
+import React, { Component } from 'react';
+import { Form, Button, Container, Row, Col } from 'react-bootstrap';
+import { Link } from 'react-router-dom'
+import { useState } from 'react';
 
 const Register = () => {
-  const [email, setEmail] = useState("");
-  const [firstName, setFirstName] = useState("");
-  const [lastName, setLastName] = useState("");
-  const [password, setPassword] = useState("");
-  const [password2, setPassword2] = useState("");
-  const [age, setAge] = useState("");
-  const [description, setDescription] = useState("");
-  const [picture, setPicture] = useState("");
-  const [loading, setLoading] = useState(false);
-  const [error, setError] = useState(false);
-  const [postSuccess, setPostSuccess] = useState(false);
 
-  const navigate = useNavigate();
-
-  const registerUser = async () => {
-    try {
-      setPostSuccess(false);
-      setError(false);
-      setLoading(true);
-
-      const newUser = {
-        firstName: firstName,
-        lastName: lastName,
-        email: email,
-        password: password,
-        age: age,
-        description: description,
-        picture: picture,
-        role: "user",
-      };
-
-      const config = {
-        method: "POST",
-        body: JSON.stringify(newUser),
-        headers: new Headers({
-          "Content-Type": "application/json",
-        }),
-      };
-
-      const response = await fetch(
-        "http://localhost:3001/users/register",
-        config
-      );
-      console.log("response:", response);
-      if (response.ok) {
-        setPostSuccess(true);
-        navigate("/login");
-      } else {
-        setError(true);
-        console.log("error:");
-      }
-    } catch (error) {
-      setError(true);
-    } finally {
-      setLoading(false);
-      setFirstName("");
-      setLastName("");
-      setEmail("");
-      setPassword("");
-      setPassword2("");
-      setAge("");
-      setDescription("");
-      setPicture("");
-      infoTimeoutFunc(3000);
-    }
-  };
-
-  const resetAllState = () => {
-    setError(false);
-    setPostSuccess(false);
-    setLoading(false);
-  };
-
-  const infoTimeoutFunc = (time) => {
-    const infoTimeout = setTimeout(resetAllState, time);
-    return () => clearTimeout(infoTimeout);
-  };
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    console.log("submitting form...");
-    if (
-      firstName &&
-      lastName &&
-      email &&
-      password &&
-      password2 &&
-      age &&
-      description &&
-      picture &&
-      !loading &&
-      !error &&
-      !postSuccess
-    ) {
-      if (password !== password2) {
-        setError(true);
-        infoTimeoutFunc(2000);
-      } else {
-        registerUser();
-      }
-    } else {
-      setError(true);
-      infoTimeoutFunc(2000);
-    }
-  };
+    const [picture, setPicture] = useState("");
 
   const handlePictureChange = (event) => {
     const file = event.target.files[0];
     setPicture(file);
-  };
+  }
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log(event.target);
+    }
+
+    const handleChange = (event) => {
+        event.preventDefault();
+        console.log(event.target);
+    }
 
   return (
     <Container>
       <Row className="justify-content-md-center">
         <Col xs lg="6">
           <h1>Register</h1>
-          <Form onSubmit={handleSubmit}>
-            <Form.Group controlId="formBasicFirstName">
-              <Form.Label>
-                First Name<span className="starz">*</span>
-              </Form.Label>
-              <Form.Control type="text" placeholder={firstName} onChange={(e) => setFirstName(e.target.value)} />
-            </Form.Group>
-            <Form.Group controlId="formBasicLastName">
-              <Form.Label>
-                Last Name<span className="starz">*</span>
-              </Form.Label>
-              <Form.Control type="text" placeholder={lastName} onChange={(e) => setLastName(e.target.value)} />
-            </Form.Group>
+          <Form onChange={handleChange}>
             <Form.Group controlId="formBasicEmail">
-              <Form.Label>
-                Email address<span className="starz">*</span>
-              </Form.Label>
-              <Form.Control type="email" placeholder={email} onChange={(e) => setEmail(e.target.value)} />
+              <Form.Label>Email address<span className="starz">*</span></Form.Label>
+              <Form.Control type="email" placeholder="Enter email" />
               <Form.Text className="text-muted">
                 We'll never share your email with anyone else.
               </Form.Text>
             </Form.Group>
+
             <Form.Group controlId="formBasicPassword">
-              <Form.Label>
-                Password<span className="starz">*</span>
-              </Form.Label>
-              <Form.Control type="password" placeholder={password} onChange={(e) => setPassword(e.target.value)} />
-            </Form.Group>
-            <Form.Group controlId="formBasicPassword">
-              <Form.Label>
-                {" "}
-                Retype password<span className="starz">*</span>
-              </Form.Label>
+              <Form.Label>Password<span className="starz">*</span></Form.Label>
               <Form.Control type="password" placeholder="Password" />
             </Form.Group>
+            <Form.Group controlId="formBasicFirstName">
+              <Form.Label>First Name<span className="starz">*</span></Form.Label>
+              <Form.Control type="text" placeholder="First Name" />
+            </Form.Group>
+            <Form.Group controlId="formBasicLastName">
+              <Form.Label>Last Name<span className="starz">*</span></Form.Label>
+              <Form.Control type="text" placeholder="Last Name" />
+            </Form.Group>
             <Form.Group controlId="formBasicAge">
-              <Form.Label>
-                Age<span className="starz">*</span>
-              </Form.Label>
-              <Form.Control type="number" placeholder={age} onChange={(e) => setAge(e.target.value)} />
+              <Form.Label>Age<span className="starz">*</span></Form.Label>
+              <Form.Control type="number" placeholder="Age" />
             </Form.Group>
             <Form.Group controlId="formBasicDescription">
-              <Form.Label>
-                Description<span className="starz">*</span>
-              </Form.Label>
-              <Form.Control as="textarea" rows={3} placeholder={description} onChange={(e) => setDescription(e.target.value)} />
+              <Form.Label>Description<span className="starz">*</span></Form.Label>
+              <Form.Control as="textarea" rows={3} placeholder="Description" />
             </Form.Group>
             <Form.Group controlId="formBasicPicture">
               <Form.Label>Picture</Form.Label>
               <Form.File
                 id="custom-file"
-                label={picture ? picture.name : "Choose file"}
+                label={picture? picture.name : "Choose file"}
                 custom
                 onChange={handlePictureChange}
               />
             </Form.Group>
-            <Button variant="primary" type="submit">
+            <Button variant="primary" type="submit" onSubmit={handleSubmit}>
               Submit
             </Button>
           </Form>
@@ -184,6 +73,6 @@ const Register = () => {
       </Row>
     </Container>
   );
-};
+}
 
 export default Register;
