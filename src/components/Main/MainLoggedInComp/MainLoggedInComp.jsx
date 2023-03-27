4a61859ba152513cb441de2660484d73955cc63b
Raulanthropos <ipsichias@yandex.com>
2023-03-27 22:42:13 +0300

    minor change before merging

--- src/components/Main/MainLoggedInComp/MainLoggedInComp.jsx
+++ src/components/Main/MainLoggedInComp/MainLoggedInComp.jsx
@@ -43,7 +43,7 @@ const Main = () => {
     <Container className='backgroundCont'>
       <Row className="d-flex align-items-center justify-content-center" style={{ flexDirection: "column" }}>
         <Col>
-          {currentUser.role === "admin" ? <><Button variant="primary" style={{width: "150px", paddingTop: "15px", marginTop: "10px"}}>Add dog</Button><Button variant="warning" style={{width: "150px", paddingTop: "15px", marginTop: "10px"}}>Edit dog</Button><Button variant="danger" style={{width: "150px", paddingTop: "15px", marginTop: "10px"}}>Delete dog</Button></> : ""}
+          {currentUser.role === "admin" ? <><Button variant="secondary" style={{width: "150px", paddingTop: "15px", marginTop: "10px"}}>Add dog</Button><Button variant="warning" style={{width: "150px", paddingTop: "15px", marginTop: "10px"}}>Edit dog</Button><Button variant="danger" style={{width: "150px", paddingTop: "15px", marginTop: "10px"}}>Delete dog</Button></> : ""}
         </Col>
         {isAuthenticated && <h2>Welcome, {currentUser.name}!</h2>}
         <Col xs="auto">
